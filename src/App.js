08ef3f638eaeb20b9f8eb61d565b0038a46a13c3
isaacwnsolomon <g00424127@atu.ie>
2024-11-29 17:27:29 +0000

    Set up basic react app with nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navigationbar';
+import Header from './components/header';
+import Footer from './components/footer';
+import Content from './components/content';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Footer/>} />
+        <Route path="/create" element={<Header/>} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
