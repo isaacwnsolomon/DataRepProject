6663cb82d381906256f7ba04bf35c16a0d146999
isaacwnsolomon <g00424127@atu.ie>
2024-11-30 13:42:01 +0000

    Main entry now expands dynamically when text goes to new line

--- src/components/diaryentry.js
+++ src/components/diaryentry.js
@@ -1,19 +1,29 @@
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 
+
 const DiaryEntry = () => {
+
+
     return (
       <Form>
       <Form.Group className="mb-3" controlId="title">
         <Form.Label>Entry Title</Form.Label>
         <Form.Control  placeholder="Enter your title" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="mainEntry">
-        <Form.Label>Entry Body</Form.Label>
-        <Form.Control  placeholder="Write whatever you feel you want too..." />
-      </Form.Group>
+      <>
+      
+      <Form.Label>Main Entry</Form.Label>
+      <FloatingLabel controlId="mainEntry" >
+        <Form.Control
+          as="textarea"
+          placeholder="Write whatever you feel like..."
+          style={{ height: '100px' }}
+        />
+      </FloatingLabel>
+    </>
     </Form>
     );
-
 
   };
   
