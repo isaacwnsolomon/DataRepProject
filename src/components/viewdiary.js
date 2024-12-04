dea53e9963d86045008fbaf68d82df4bc5b400ff
isaacwnsolomon <g00424127@atu.ie>
2024-12-04 17:57:03 +0000

    Working search bar

--- src/components/viewdiary.js
+++ src/components/viewdiary.js
@@ -1,11 +1,18 @@
 import Diary from "./diary"; // Importing diary component
 import { useEffect, useState } from "react";// Importing react hooks
 import axios from "axios"; // Importing axios for http requests
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // Define read component
 const ViewDiary = () => {
-//State variable movies to store entry data and setEntry function to update
+//State variable movies to store entry data and setEntry function to update and search query
   const [entries, setEntry] = useState([]);
+  const [query, setQuery] = useState('');
 
   //Fetch data from api and update entries state
   const reloadData = ()=>{
@@ -27,14 +34,41 @@ const ViewDiary = () => {
       reloadData();
   },[]);
 
+    // Filter entries based on the search query
+    const filteredEntries = entries.filter((entry) =>
+      entry.title.toLowerCase().includes(query.toLowerCase()) || // Search in title
+      entry.entry.toLowerCase().includes(query.toLowerCase())    // Search in entry content
+    );
+
+      // Handle search input change
+  const handleSearchChange = (e) => {
+    setQuery(e.target.value); // Update the search query
+  };
   // Render component
   return (
     <div className="app-container">
-    <div>
-      <Diary myDiary={entries} ReloadData={reloadData} />
-    </div>
+      <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+              value={query} 
+              onChange={handleSearchChange}
+            />
+          </Col>
+      
+        </Row>
+      </Form>
+      {/* Render Diary Component with Filtered Entries */}
+      <div>
+        <Diary myDiary={filteredEntries} ReloadData={reloadData} /> {/* Pass filteredEntries */}
+      </div>
     </div>
   );
+  
+  
 }
 // export ViewDiary component
 export default ViewDiary;
